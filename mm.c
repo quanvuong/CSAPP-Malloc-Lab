9153d984abb83b9cb7ace83bcf0c7e41b1e3f89a
quanvuong <quan.vuong@nyu.edu>
2016-11-30 01:22:03 +0400

    minor changes

--- mm.c
+++ mm.c
@@ -88,15 +88,34 @@ team_t team = {
 #define BP_LARGER_EQUAL(bp, size) (GET_SIZE(HDRP(bp)) >= size)
 
 // Global variables and function declarations
+void * prolog_p; // point to the start of the ftr of prolog
 
-static void *coalesce(void *bp);
 static void * extend_heap(size_t bytes);
+static void add_free_block(void *bp);
+// static int which_free_list(size_t s);
+static void remove_free_block(void *bp);
+static void place(void *bp, size_t requested_size);
+static void *find_fit(size_t size);
+static void *coalesce(void *ptr);
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    /* Create heap basic prologue, epilogue */
+    if ((prolog_p = mem_sbrk(4*WSIZE)) == (void *) -1) return -1;
+
+    PUT(prolog_p, 0); /* Empty padding block */
+    PUT(prolog_p + (1*WSIZE), PACK(DSIZE, 1)); /* Prologue header */
+    PUT(prolog_p + (2*WSIZE), PACK(DSIZE, 1)); /* Prologue header */
+    PUT(prolog_p + (3*WSIZE), PACK(0, 1)); /* Epilogue header */
+    prolog_p += (2*WSIZE); /* Point prolog_p to Prologue */
+
+    /* Allocate additional space to the heap */
+    if (extend_heap(CHUNKSIZE) == NULL) 
+        return -1;
+
     return 0;
 }
 
@@ -106,21 +125,42 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
+    if (size == 0) return NULL;
+
+    size_t extend_size;
+    size_t asize;
+    void * bp;
+
+    // Adjust block size to include overhead and alignment reqs 
+    if (size <= DSIZE) asize = DSIZE * 2;
     else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
     }
+
+    /* Search free lists for a fit */
+    if ((bp = find_fit(asize)) != NULL) {
+        place(bp, asize);
+        return bp;
+    }
+
+    /* No fit found. We get more memory and place the block */
+    extend_size = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extend_size)) == NULL) 
+        return NULL;
+    place(bp, asize);
+
+    return bp;
 }
 
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
+    unsigned int size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0)); 
+	PUT(FTRP(bp), PACK(size, 0));
 }
 
 /*
@@ -190,35 +230,108 @@ static void *coalesce(void *bp)
         bp = PREV_BLKP(bp);
     }
 
+    add_free_block(bp);
+
     return bp;
 }
 
 static void * extend_heap(size_t bytes)
 {
-    void * ptr;
+    void * bp;
     size_t size = ALIGN(bytes);
 
-    if ((ptr = mem_sbrk(size)) == (void *) -1)
+    if ((bp = mem_sbrk(size)) == (void *) -1)
         return NULL;
     
     // Initialize the new block header, footer, and new epilog
-    PUT(HDRP(ptr), PACK(size, 0)); // header
-    PUT(FTRP(ptr), PACK(size, 0)); // footer
-    PUT(HDRP(NEXT_BLKP(ptr)), PACK(0, 1)); // epilog, size 1, allocated
+    PUT(HDRP(bp), PACK(size, 0)); // header
+    PUT(FTRP(bp), PACK(size, 0)); // footer
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // epilog, size 1, allocated
 
-    add_free_block(ptr);
+    add_free_block(bp);
 
-    return coalesce(ptr);
+    return coalesce(bp);
+}
+
+static void *find_fit(size_t size)
+{
+    void * bp = NEXT_BLKP(prolog_p);
+
+    while (GET_ALLOC(HDRP(bp)) && GET_SIZE(HDRP(bp)) < size && GET_SIZE(HDRP(bp)) > 0) 
+        bp = NEXT_BLKP(bp);
+
+    if (GET_SIZE(HDRP(bp))) return NULL;
+
+    return bp;
+}
+
+static void add_free_block(void *bp)
+{
+    unsigned int size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0)); 
+	PUT(FTRP(bp), PACK(size, 0));
+}
+
+static void remove_free_block(void *bp)
+{
+    unsigned int size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 1)); 
+	PUT(FTRP(bp), PACK(size, 1));
+}
+
+static void place(void *bp, size_t requested_size)
+{
+    remove_free_block(bp);
+
+    PUT(HDRP(bp), PACK(requested_size, 1));
+    PUT(FTRP(bp), PACK(requested_size, 1));
 }
 
 
-
-
-
-
-
-
-
-
-
+// static void *coalesce(void *ptr)
+// {
+//   size_t prev_alloc = GET_ALLOC(HDRP(PREV_BLKP(ptr)));
+//   size_t next_alloc = GET_ALLOC(HDRP(PREV_BLKP(ptr)));
+//   size_t size = GET_SIZE(HDRP(ptr));
+  
+//   /* Return if previous and next blocks are allocated */
+//   if (prev_alloc && next_alloc) {
+//     return ptr;
+//   }
+  
+//   /* Do not coalesce with previous block if it is tagged */
+//   if (GET_TAG(HDRP(PREV_BLKP(ptr))))
+//     prev_alloc = 1;
+  
+//   /* Remove old block from list */
+//   remove_free_block(ptr);
+  
+//   /* Detect free blocks and merge, if possible */
+//   if (prev_alloc && !next_alloc) {
+//     remove_free_block(PREV_BLKP(ptr));
+//     size += GET_SIZE(HDRP(PREV_BLKP(ptr)));
+//     PUT(HDRP(ptr), PACK(size, 0));
+//     PUT(FTRP(ptr), PACK(size, 0));
+//   } else if (!prev_alloc && next_alloc) {
+//     remove_free_block(PREV_BLKP(ptr));
+//     size += GET_SIZE(HDRP(PREV_BLKP(ptr)));
+//     PUT(FTRP(ptr), PACK(size, 0));
+//     PUT(HDRP(PREV_BLKP(ptr)), PACK(size, 0));
+//     ptr = PREV_BLKP(ptr);
+//   } else {
+//     remove_free_block(PREV_BLKP(ptr));
+//     remove_free_block(PREV_BLKP(ptr));
+//     size += GET_SIZE(HDRP(PREV_BLKP(ptr))) + GET_SIZE(HDRP(PREV_BLKP(ptr)));
+//     PUT(HDRP(PREV_BLKP(ptr)), PACK(size, 0));
+//     PUT(FTRP(PREV_BLKP(ptr)), PACK(size, 0));
+//     ptr = PREV_BLKP(ptr);
+//   }
+  
+//   /* Adjust segregated linked lists */
+//   add_free_block(ptr);
+  
+//   return ptr;
+// }
 
